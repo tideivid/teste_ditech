f09e69c721e94133a072452d3bbc60392eb18030
Deividi <ti.deividi@gmail.com>
2017-03-08 10:52:03 -0300

    Criação dos aquivos default

---
+++ views/login/index.php
@@ -0,0 +1,27 @@
+
+<div class="container">
+    <div class="row vertical-offset-100">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">                                
+                    <div class="row-fluid user-row">
+                        <img src="http://s11.postimg.org/7kzgji28v/logo_sm_2_mr_1.png" class="img-responsive" alt="Admin"/>
+                    </div>
+                </div>
+                <div class="panel-body">
+                    <form accept-charset="UTF-8" role="form" class="form-signin" method="post" action="">
+                        <fieldset>
+                            <label class="panel-login">
+                                <div class="login_result"></div>
+                            </label>
+                            <input class="form-control" placeholder="Usuario" id="username" type="text">
+                            <input class="form-control" placeholder="Senha" id="password" type="password">
+                            <br></br>
+                            <input class="btn btn-lg btn-success btn-block" type="submit" id="login" value="Login »">
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
