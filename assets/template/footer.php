d03e6cd2af94c9d767043ff3c985728ebb11c2a8
Deividi <ti.deividi@gmail.com>
2017-03-08 10:43:57 -0300

    Estrutura de diretorios e arquivos

---
+++ assets/template/footer.php
@@ -0,0 +1,12 @@
+    <!-- jQuery Version 1.11.1 -->
+    <script src="application/lib/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="application/lib/js/bootstrap.min.js"></script>
+
+    <!-- Custom js-->
+ 	<script src="application/assets/js/form_login.js"></script>
+
+</body>
+
+</html>
